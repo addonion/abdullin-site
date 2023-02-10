930fbb576c5ed67d3075154c75b57cf713d26a77
addonion <addonion@gmail.com>
2023-02-10 08:22:01 +0400

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  adapter: netlify(),
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })]
 });
